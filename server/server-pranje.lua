4402b57db60e0cd25867ac2e3bebfc56778da167
AlminBP76 <71565182+AlminBP76@users.noreply.github.com>
2022-03-03 00:48:01 +0100

    Dodani Logovi za Pranje Novca
    Dodajte svoj Webhook u server/server-pranje.lua linija 36

--- server/server-pranje.lua
+++ server/server-pranje.lua
@@ -22,9 +22,29 @@ AddEventHandler('71Pranje:operiPare',function(kolicinaPranje)
         local brisanje = kolicinaPranje / 100 * provizija
         local cistak = kolicinaPranje - brisanje
         igrac.addMoney(cistak)
+	posaljiLogMrtvi('Pranje Novca', GetPlayerName(soruce) .. ' je oprao novac i dobio '..cistak..'€!')
         TriggerClientEvent("esx:showNotification", source, "Vase pare su oprane dobili ste: "..cistak.."€!")
     else
         igrac.addMoney(kolicinaPranje)
+	posaljiLogMrtvi('Pranje Novca', GetPlayerName(soruce) .. ' je oprao novac i dobio '..kolicinaPranje..'€!')
         TriggerClientEvent("esx:showNotification", source, "Vase pare su oprane dobili ste: "..kolicinaPranje.."€!")
     end
 end)
+
+function posaljiLogMrtvi(name,message, color)
+	local vreme = os.date("*t")
+	local DiscordWebHook = "OVDJE WEBHOOK"
+	local embeds = {
+		{
+			["title"]=message,
+			["type"]="rich",
+			["color"] =color,
+			["footer"]=  {
+		  ["text"]= "Vreme: " .. vreme.hour .. ":" .. vreme.min .. ":" .. vreme.sec,
+  
+		   },
+		}
+	} 
+	if message == nil or message == '' then return FALSE end
+	PerformHttpRequest(DiscordWebHook, function(err, text, headers) end, 'POST', json.encode({ username = name, embeds = embeds}), { ['Content-Type'] = 'application/json' })
+end
